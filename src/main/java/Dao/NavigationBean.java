b7fb4008d7d19a7e08fd4e3279421f40ffc8ca20
RSThiang <thiangrokhysiga@gmail.com>
2022-12-15 16:48:06 +0000

    update

--- src/main/java/Dao/NavigationBean.java
+++ src/main/java/Dao/NavigationBean.java
@@ -8,24 +8,46 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
-@Named
-@RequestScoped
+@Named(value = "NavigationBean")
 
+@ViewScoped
 public class NavigationBean implements Serializable {
+
+
     private static final long serialVersionUID = 1L;
-     private String page="template.xhtml";
+
+    private String page;
     @PostConstruct
-    public void init() {}
-public NavigationBean(String page){
-        this.page = page;
- }
+     public void Init(){
+     }
 
     public String getPage() {
         return page;
     }
 
-
     public void setPage(String currentPage) {
         this.page=currentPage;
     }
+
+//    private String url;
+//
+//    public NavigationBean() {
+//     }
+//     @PostConstruct
+//     public void Init(){
+//
+//     }
+//     public void CheckUrl(String url){
+//        this.url= url;
+//     }
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+
+
 }
